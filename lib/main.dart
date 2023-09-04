2925d94d25d52a0b61b2d4aa8fc7ea3c78f1a320
ravshan1119 <allamurodovravshan38@gmail.com>
2023-09-04 21:09:09 +0500

    hometack

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 
 import 'package:bloc_pratice/cubits/cubit.dart';
 import 'package:bloc_pratice/ui/veiw.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const App());
 }
 
--- lib/ui/veiw.dart
+++ lib/ui/veiw.dart
@@ -1,7 +1,5 @@
-import 'package:bloc_pratice/cubits/cubit.dart';
-import 'package:bloc_pratice/cubits/view_state.dart';
+import 'package:bloc_pratice/ui/detail.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ViewScreen extends StatefulWidget {
   const ViewScreen({super.key});
@@ -17,43 +15,13 @@ class _ViewScreenState extends State<ViewScreen> {
       appBar: AppBar(
         title: const Text("nimadur"),
       ),
-      body: BlocBuilder<CubitView, ViewState>(
-        builder: (context, state) {
-          return Column(
-            children: [
-              SizedBox(
-                height: 50,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    ...List.generate(
-                        10,
-                        (index) => Padding(
-                              padding: const EdgeInsets.all(1),
-                              child: GestureDetector(
-                                onTap: () {
-                                  context.read<CubitView>().change(index);
-                                  print(context.read<CubitView>().list);
-                                },
-                                child: Container(
-                                  height: 40,
-                                  width: 100,
-                                  color: context
-                                          .read<CubitView>()
-                                          .list
-                                          .contains(index)
-                                      ? Colors.grey
-                                      : Colors.blue,
-                                ),
-                              ),
-                            ))
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>const DetailScreen()));
+          }, child: const Text("Click"))
+        ],
+      )
     );
   }
 }
